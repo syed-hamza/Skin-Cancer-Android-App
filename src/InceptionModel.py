c35f23e6c1a5af5bbe8d5719cfd1219b0b5ecbe8
syed-hamza <53032653+syed-hamza@users.noreply.github.com>
2019-11-25 19:15:53 +0530

    Update InceptionModel.py

--- src/InceptionModel.py
+++ src/InceptionModel.py
@@ -2,9 +2,6 @@ lr =0.0001
 def create_inception_model2(n_classes=8,learning_rate=lr):
     raw_model = keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_shape=(224,224,3), classes=n_classes)
 # first: train only the top layers (which were randomly initialized)
-# i.e. freeze all convolutional InceptionV3 layers
-    #for layer in raw_model.layers:
-    #    layer.trainable = False
     full_model = Sequential()
     full_model.add(raw_model)
     full_model.add(GlobalAveragePooling2D())
@@ -15,3 +12,4 @@ def create_inception_model2(n_classes=8,learning_rate=lr):
     full_model.compile(Adam(lr=learning_rate), loss = 'categorical_crossentropy', metrics = ['accuracy'])
     full_model.summary()
     return full_model
+model = create_inception_model2(n_classes=8,learning_rate=lr)
