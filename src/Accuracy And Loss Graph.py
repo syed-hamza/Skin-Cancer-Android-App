e9e021cdcd55a64747d04cdba04f7f8e62cf2352
syed-hamza <53032653+syed-hamza@users.noreply.github.com>
2019-11-25 19:19:00 +0530

    Create Accuracy And Loss Graph.py

---
+++ src/Accuracy And Loss Graph.py
@@ -0,0 +1,15 @@
+# summarize history for accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
